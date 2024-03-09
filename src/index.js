2c6aa79a1d13c7607ef8c0f7671d4819aaec3d16
Песецкий Никита Андреевич <95304979+FFrein@users.noreply.github.com>
2024-03-09 14:07:06 +0300

    host web

--- src/index.js
+++ src/index.js
@@ -32,3 +32,31 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, "Received your message");
     }
 });
+
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        try {
+            const stat = fs.stat(fname, (err, stat)=>{
+
+                res.writeHead(200, {
+                    'Content-Type': 'image/png', 
+                    'Content-Length': stat.size
+                });
+    
+                const stream = fs.createReadStream(fname);
+                stream.pipe(res);
+            });
+
+        } catch (error) {
+            res.statusCode = 500;
+            res.end('Internal Server Error. Error: ' + error);
+        }
+    } else {
+        res.end(req.url);
+    }
+});
+
+server.listen(PORT, () => console.log('Server started on PORT ' + PORT));
