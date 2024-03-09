972c5b517241ce1226414638011d2f6c18dbe01a
Песецкий Никита Андреевич <95304979+FFrein@users.noreply.github.com>
2024-03-09 23:22:23 +0300

    test

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -12,11 +12,27 @@ const Game = ()=>{
         });
     });
 
+    function test2(){
+        let tg3 = window.Telegram.WebApp;
+        tg3.MainButton.show();
+        tg3.MainButton.enable();
+        tg3.onEvent('mainButtonClicked', function(){
+            tg3.sendData("some string that we need to send"); 
+        });
+    }
+
+    function test3(){
+        let tg2 = window.Telegram.WebApp;
+        tg2.sendData("test data");
+    }
+
     return(
         <div>
             <p>Game</p>
             <a href="/">lobby</a>
             <button onclick={onToggleButton} className="button">active</button>
+            <button onclick={test2()} className="button">active2</button>
+            <button onclick={test3()} className="button">active3</button>
         </div>
     )
 }
